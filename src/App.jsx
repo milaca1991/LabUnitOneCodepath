cfde12fc2cab35e97436f72da540047a24ef6b88
unknown <milaca1991@hotmail.com>
2023-03-02 19:56:44 -0500

    new project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+
+        <h1>Calendario de estudio</h1>
+          <h2>Bienvenido, Este será tu nuevo Calendario</h2>
+          
+
+    </div>
+  )
+}
+export default App
